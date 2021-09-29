fdac2d45f912e394feff0d07e279d3d27f502449
Karolin Rafalski <kr2174@caa.columbia.edu>
2021-09-29 16:30:20 -0400

    something weird

---
+++ solution.js
@@ -0,0 +1,75 @@
+const { nums, words } = require("./data/data.js");
+
+// Every
+const isEveryNumGreaterThan2 = () => {
+  //
+};
+
+const isEveryWordShorterThan7 = () => {
+  //
+};
+
+// Filter
+
+const arrayLessThan5 = () => {
+  //
+};
+
+const arrayOddLengthWords = () => {
+  //
+};
+
+// Find
+
+const firstValDivisibleBy4 = () => {
+  //
+};
+
+const firstWordLongerThan4Char = () => {
+  //
+};
+
+// For Each
+
+const logValuesTimes3 = () => {
+  //
+};
+
+const logWordsWithExclamation = () => {
+  //
+};
+
+// Map
+
+const arrayValuesTimes100TimesIndex = () => {
+  //
+};
+
+const arrayWordsUpcased = () => {
+  //
+};
+
+// Some
+
+const areSomeNumsDivisibleBy7 = () => {
+  //
+};
+
+const doSomeWordsHaveAnA = () => {
+  //
+};
+
+module.exports = {
+  isEveryNumGreaterThan2,
+  isEveryWordShorterThan7,
+  arrayLessThan5,
+  arrayOddLengthWords,
+  firstValDivisibleBy4,
+  firstWordLongerThan4Char,
+  logValuesTimes3,
+  logWordsWithExclamation,
+  arrayValuesTimes100TimesIndex,
+  arrayWordsUpcased,
+  areSomeNumsDivisibleBy7,
+  doSomeWordsHaveAnA,
+};
